f0dd3762b77d9b2843395a320b9cfaf2b7b7c10f
shanghai-Jerry <icbigdata@gmail.com>
2021-07-27 19:48:34 +0800

    update

--- leetcode/Entry.go
+++ leetcode/Entry.go
@@ -2,13 +2,11 @@ package leetcode
 
 import (
 	"fmt"
-
-	"github.com/bigchange/go-practice/leetcode/tutorials"
 )
 
 func Entry() {
-	nums := []int{3,5,10,1,8,2,9,4,11,7,12,6}
-	ret := tutorials.RandomSelect(nums, 8)
-	fmt.Println("nums:", nums)
+	// nums := []int{3,5,10,1,8,2,9,4,11,7,12,6}
+	ret := findMaximizedCapital(3,0 , []int{1,2,3}, []int{0, 1,2})
+	fmt.Println("ret:", ret)
 	fmt.Printf("leetCode Entry return:%+v",  ret)
 }
--- main.go
+++ main.go
@@ -23,6 +23,17 @@ func leetCodeEntry() {
 	leetcode.Entry()
 }
 
+func main() {
+	leetCodeEntry()
+	TestF()
+	// meituan()
+	// TestSyncPool()
+	// DecoratedVisitorLoadFile()
+	// NormalVisitor()
+	tutorialsEntry()
+
+}
+
 func meituan ()  {
 	var n int
 	fmt.Scanln(&n)
@@ -41,14 +52,41 @@ func meituan ()  {
 	}
 }
 
-func main() {
-	TestF()
-	meituan()
-	// TestSyncPool()
-	// DecoratedVisitorLoadFile()
-	// NormalVisitor()
-	tutorialsEntry()
-	leetCodeEntry()
+func IsValid(s string) bool {
+	getLetter := false
+	getNum := false
+	for i, c := range s {
+		if i == 0 {
+			if !IsLetter(c) {
+				return false
+			}
+			getLetter = true
+		}
+
+		if !IsLetter(c) && !IsNumeric(c) {
+			return false
+		}
+		if IsNumeric(c) {
+			getNum = true
+		}
+	}
+	if getLetter && getNum {
+		return true
+	}
+	return false
+}
+
+func IsLetter(c rune) bool {
+	if ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') {
+		return true
+	}
+	return false
+}
+func IsNumeric(c rune) bool {
+	if '0' <= c && c <= '9' {
+		return true
+	}
+	return false
 }
 
 
@@ -89,6 +127,7 @@ func PrimeFilter(ctx context.Context, in <-chan int, prime int) chan int {
 	}()
 	return out
 }
+
 
 var pool *sync.Pool
 type Person struct {
