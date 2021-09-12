e04da4d85f18893618ecaaa024a0d209a5d5bae1
shanghai-Jerry <icbigdata@gmail.com>
2021-09-12 11:23:33 +0800

    update

--- leetcode/Entry.go
+++ leetcode/Entry.go
@@ -1,14 +1,18 @@
 package leetcode
 
 import (
+	"flag"
 	"fmt"
 )
 
 func Entry() {
 	// nums := []int{3,5,10,1,8,2,9,4,11,7,12,6}
-	ret := chalkReplacer([]int{3, 4, 1, 2}, 25)
+	var s string
+	flag.StringVar(&s, "s","","input string")
+	flag.Parse()
+	ret := checkValidString(s)
 
 	fmt.Printf("ret:[%v]\n",ret)
 
-	fmt.Printf("leetCode Entry return:%+v",  ret)
+	fmt.Printf("leetCode Entry return:%v \n",  ret)
 }
