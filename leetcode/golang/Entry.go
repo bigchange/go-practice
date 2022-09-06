0c20f0645550008a09968919b98a274b3f8ebe20
shanghai-Jerry <icbigdata@gmail.com>
2022-09-06 18:32:37 +0800

    update

--- leetcode/golang/Entry.go
+++ leetcode/golang/Entry.go
@@ -10,28 +10,6 @@ func Entry() {
 	var s string
 	flag.StringVar(&s, "s", "", "input string")
 	flag.Parse()
-	nodes  := findDuplicateSubtrees(&TreeNode{
-		Val: 5,
-		Left: &TreeNode{
-			Val: 2,
-			Right: &TreeNode{
-				Val: 4,
-			},
-			Left: &TreeNode{
-				Val: 1,
-			},
-		},
-		Right: &TreeNode{
-			Val: 2,
-			Right: &TreeNode{
-				Val: 4,
-			},
-			Left: &TreeNode{
-				Val: 1,
-			},
-		},
-	})
-	for _,i := range nodes {
-		fmt.Println("res:", printTree(i))
-	}
+	total  := uniqueLetterString_2("LEETCODE")
+	fmt.Println("total", total)
 }
