2aedd7ec57e7ca70f4f16c2f91967bdf71e16ea6
shanghai-Jerry <icbigdata@gmail.com>
2022-09-17 22:29:49 +0800

    672

--- leetcode/golang/Entry.go
+++ leetcode/golang/Entry.go
@@ -8,12 +8,16 @@ import (
 func Entry() {
 	// nums := []int{3,5,10,1,8,2,9,4,11,7,12,6}
 	var s string
+	var n int
+	var p int
+	flag.IntVar(&n, "n", 1, "input n")
+	flag.IntVar(&p, "p", 1, "input p")
 	flag.StringVar(&s, "s", "", "input string")
 	flag.Parse()
 	//q := []int{10, 20, 5}
 	//w := []int{70, 50, 30}
 	//k := 2
-	res := trimMean([]int{})
+	res := flipLights(n,p)
 	fmt.Println("res", res)
 }
 
